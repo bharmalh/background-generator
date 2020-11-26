66b7e16a3df1d084e19db748ad16ce90c4629445
Hussain Bharmal <47472219+bharmalh@users.noreply.github.com>
2020-11-27 01:18:37 +0300

    cleared node mess

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var _=require('lodash');
+
+var array = [1,2,3,4,5,6,7,8];
+console.log('answer:', _.without(array, 3));
 var col1 = document.querySelector("#color1");
 var col2 = document.querySelector("#color2");
 var css = document.querySelector("h3");
